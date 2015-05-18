285bec763f7cc7eec3ec6fa749a7cae22348b6ea
Polly Sileo <pollysileo@gmail.com>
2015-05-18 14:47:54 -0400

    homework complete

--- triangle_test.rb
+++ triangle_test.rb
@@ -2,9 +2,28 @@
 # 2. Run `ruby triangle_test.rb`. You should see 6 error messages.
 # 3. Implement the Triangle class until all 6 tests are passing.
 require "minitest/autorun"
+require 'pry'
 
 class Triangle
-  # Add your code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    if (@side1 == @side2 && @side2 == @side3)
+      return :equilateral
+    elsif (@side1 == @side2 || @side1 == @side3 || @side2 == @side3)
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+
+  def perimeter
+    return @side1 + @side2 + @side3
+  end
 end
 
 class TestMeme < Minitest::Test
@@ -29,7 +48,8 @@ class TestMeme < Minitest::Test
   end
 
   def test_perimeter_right
-    assert_equal Triangle.new(3,4,5), t.perimeter
+    t = Triangle.new(3,4,5)
+    assert_equal 12, t.perimeter
   end
 
   def test_perimeter_eq
